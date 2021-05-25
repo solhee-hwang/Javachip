2a2afb7356af747aa5bb41638cc9de3a19a2e65b
solhee-hwang <66052043+solhee-hwang@users.noreply.github.com>
2021-05-26 00:37:08 +0900

    몬스터,여러던전
    여러 몬스터 및 던전 구현, user에서 공격을 완성하면 몬스터 공격 합칠예정

--- src/play_game.java
+++ src/play_game.java
@@ -18,13 +18,44 @@ public class play_game {
 		int place = c.nextInt();
 
 		if (place == 1) {
-			// 던전입장
+			System.out.println("============player choice==============");
+			System.out.println("(1).level_1 던전|| (2).level_2 던전|| (3).level_3 던전||(4).던전에서 나가기");
+			System.out.print(">");
+			int player_ch = c.nextInt();
+			permission(j.level, player_ch);
+
+			
 		} else if (place == 2) {
 			// 상점입장
 		} else {
 			System.out.println("1,2에 해당하는 숫자만 입력해주세요.");
 		}
 
+	}
+	public static void permission(int level, int player_ch){
+		
+		switch(player_ch){
+				case 1: 
+					System.out.println("level 1 던전에 입장하셨습니다.");
+					break;
+					//던전플레이 함수 실행
+				case 2:
+					if(level <10){
+						System.out.println("level 10이상 부터 입장이 가능합니다.");
+						//게임 플레이 함수로 감
+						break;
+					}
+					System.out.println("level_2 던전에 입장하셨습니다.");
+					break;
+				case 3:
+					if(level < 20){
+						System.out.println("level 20이상부터 입장이 가능합니다.");
+						break;
+					}
+					System.out.println("level_3 던전에 입장하셨습니다.");
+				case 4:
+					System.out.println(">>던전에서 나갔습니다.");
+				}
 	}
 
 }
